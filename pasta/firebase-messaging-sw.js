10c01cf316bc11f694ba0ac71df94b7d3ca721fe
jmarinho-egoi <74199757+jmarinho-egoi@users.noreply.github.com>
2022-09-02 12:23:49 +0100

    Update firebase-messaging-sw.js

--- pasta/firebase-messaging-sw.js
+++ pasta/firebase-messaging-sw.js
@@ -11,8 +11,8 @@
  * All rights reserved. 2021
  *
  */
-importScripts('https://www.gstatic.com/firebasejs/6.0.2/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/6.0.2/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/7.5.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/7.5.0/firebase-messaging.js');
 
 'use strict';
 firebase.initializeApp({
